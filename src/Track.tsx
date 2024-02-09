a82632a469fc578269e770f5b378ffb90da8f9cc
stevebarakat <stevebarakat@gmail.com>
2024-02-08 19:06:43 -0500

    check in

--- src/Track.tsx
+++ src/Track.tsx
@@ -1,11 +1,11 @@
 import { useSelector } from "@xstate/react";
 
 function Track({ track, onRemove }) {
-  const { name } = useSelector(track, (s) => s.context);
+  const { name } = useSelector(track, (state) => state.context);
   return (
     <div>
-      {/* {name} */}
       <button onClick={onRemove}>x</button>
+      {name}
     </div>
   );
 }
--- src/machines/trackMachine.ts
+++ src/machines/trackMachine.ts
@@ -1,72 +1,20 @@
-import { createMachine, assign, fromPromise } from "xstate";
+import { createMachine } from "xstate";
 
 export const trackMachine = createMachine({
   id: "track",
   types: {} as {
     context: {
-      prevName: string;
       name: string;
     };
-    events:
-      | {
-          type: "SET_NAME";
-          value: string;
-        }
-      | {
-          type: "SAVE";
-        }
-      | {
-          type: "EDIT";
-        }
-      | {
-          type: "CANCEL";
-        };
     input: {
       name: string;
     };
   },
   initial: "reading",
   context: ({ input }) => ({
-    prevName: input.name,
     name: input.name,
   }),
   states: {
-    reading: {
-      tags: "read",
-      on: {
-        EDIT: "editing",
-      },
-    },
-    editing: {
-      tags: "form",
-      on: {
-        SET_NAME: {
-          actions: assign({ name: ({ event }) => event.value }),
-        },
-        SAVE: {
-          target: "saving",
-        },
-      },
-    },
-    saving: {
-      tags: ["form", "saving"],
-      invoke: {
-        src: fromPromise(async () => {
-          // Simulate network request
-          await new Promise((resolve) => setTimeout(resolve, 1000));
-          return true;
-        }),
-        onDone: {
-          target: "reading",
-          actions: assign({ prevName: ({ context }) => context.name }),
-        },
-      },
-    },
-  },
-  on: {
-    CANCEL: {
-      actions: assign({ name: ({ context }) => context.prevName }),
-      target: ".reading",
-    },
+    reading: {},
   },
 });
